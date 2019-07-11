118a0d9172b7a749055dedfb5b51587192dfd2a7
Kristin Barr <kristinbarr@gmail.com>
2019-07-11 17:11:27 -0400

    steps 1-4 completed

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,30 +1,38 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+/* Step 1: using axios, send a GET request to the following URL
+  (replacing the palceholder with your Github name):
+  https://api.github.com/users/<your name>
 */
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+axios.get('https://api.github.com/users/kristinbarr')
+  .then((data) => {
+    const cards = document.querySelector('.cards')
+    cards.appendChild(myFunc(data.data))
+  })
 
-   Skip to Step 3.
+
+/* Step 2: Inspect and study the data coming back, this is YOUR
+  github info! You will need to understand the structure of this
+  data in order to use it to build your component function
+
+  Skip to Step 3.
 */
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
+/* Step 4: Pass the data received from Github into your function,
+  create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
+/* Step 5: Now that you have your own card getting added to the DOM, either
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers
+          , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
-          
+
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = []
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -35,7 +43,7 @@ const followersArray = [];
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -43,10 +51,52 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
 
-/* List of LS Instructors Github username's: 
+function myFunc(obj) {
+
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const pageUrl = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  card.appendChild(img)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  profile.appendChild(pageUrl)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+  card.classList.add('card')
+  img.setAttribute('src', `${obj.avatar_url}`)
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  name.textContent = obj.name
+  username.classList.add('username')
+  username.textContent = obj.login
+  location.textContent = `Location: ${obj.location}`
+  profile.textContent = `Profile: `
+  pageUrl.setAttribute('href', `${obj.html_url}`)
+  pageUrl.textContent = `${obj.html_url}`
+  followers.textContent = `Followers: ${obj.followers}`
+  following.textContent = `Following: ${obj.following}`
+  bio.textContent = `${obj.bio}`
+
+  return card
+}
+
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
